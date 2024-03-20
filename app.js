955ecf9def2b39a2f2b3d77b51e03cb3475adaeb
nymka1997 <dagwanymka@gmail.com>
2024-03-21 00:00:02 +0800

    click enter hiih

---
+++ app.js
@@ -0,0 +1,32 @@
+// Дэлэгцэн дээр ажилах контролор
+var uiController =(function(){
+
+})();
+
+// Санхүүтэй дээр ажилах контролор
+var finaceController =(function(){
+
+})();
+
+
+// Програмын дээр ажилах контролор
+var appController =(function(){
+    
+    var ctrlAddItem = function(){
+
+        console.log("darlaa")
+    }
+
+
+    document.querySelector(".add__btn").addEventListener("click",function(){
+        ctrlAddItem();
+    });
+
+
+    document.addEventListener("keypress",function(event){
+       if (event.keyCode === 13 || event.which === 13) {
+         ctrlAddItem();
+       }
+    });
+     
+})();
